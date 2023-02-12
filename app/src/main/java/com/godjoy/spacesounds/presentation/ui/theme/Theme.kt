f010f339e7e6819ccc7bc161564d01e3d1568256
godjoy <tlschgml556@gmail.com>
2023-02-12 23:17:51 +0900

    dependency 추가 - navigation : 앱 탐색 - hilt navigation : Navigation Compose에 viewmodel 주입 - coil : 이미지 처리 Application class 이름 변경 - App -> Application 더 명확하게 변경 presentation 폴더 추가

--- app/src/main/java/com/godjoy/spacesounds/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/godjoy/spacesounds/presentation/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.godjoy.spacesounds.ui.theme
+package com.godjoy.spacesounds.presentation.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
